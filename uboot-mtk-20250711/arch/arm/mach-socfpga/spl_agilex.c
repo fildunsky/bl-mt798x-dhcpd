e58d9edcd164026c57361f9f1489a8191dd7b559
Ye Li <ye.li@nxp.com>
2025-09-12 17:41:11 +0800

    arm: armv8: Fix spl recover data section broken
    SPL recover data section is broken which causes reboot failure on
    some i.MX platforms (iMX8QM/iMX95).
    The global variable cold_reboot_flag is assigned to weak reset_flag
    function which always return 1, so restore never been executed in
    warm reboot.
    
    Fixes: 1c37e59bfbba ("arm: armv8: Improve SPL data save and restore implementation")
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- uboot-mtk-20250711/arch/arm/mach-socfpga/spl_agilex.c
+++ uboot-mtk-20250711/arch/arm/mach-socfpga/spl_agilex.c
@@ -23,6 +23,20 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+u32 reset_flag(u32 flag)
+{
+	/* Check rstmgr.stat for warm reset status */
+	u32 status = readl(SOCFPGA_RSTMGR_ADDRESS);
+
+	/* Check whether any L4 watchdogs or SDM had triggered warm reset */
+	u32 warm_reset_mask = RSTMGR_L4WD_MPU_WARMRESET_MASK;
+
+	if (status & warm_reset_mask)
+		return 0;
+
+	return 1;
+}
+
 void board_init_f(ulong dummy)
 {
 	int ret;
